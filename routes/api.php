3a48406742261978535911fbab3621351587929a
Jorgito Ramírez Galvan <jorgeramirezgalvan99@gmail.com>
2024-02-20 18:35:38 +0100

    Adoptions

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,3 +34,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::patch('/user', [UserController::class, 'updateUser']);
     Route::delete('/user', [UserController::class, 'deleteUser']);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ------------ ADOPTERS -------------
